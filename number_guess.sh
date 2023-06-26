33a3fb92d7383e8ca85b43f5bc09e6575d1ec943
Carlos <134751214+cleall@users.noreply.github.com>
2023-06-26 12:34:03 -0600

    Update number_guess.sh v1.0.0
    This is a working version of number_guess.sh though idk why code road extension still does not mark all tasks as completed and sometimes it unmarks tasks that were previously marked as correct.

--- number_guess.sh
+++ number_guess.sh
@@ -6,87 +6,76 @@ if [ -f .env ]; then
 fi
 
 # define PSQL variable to query db
-PSQL="psql -X --username=$USERNAME --dbname=$DB_NAME --tuples-only -c"
-
-# get random number 1-1000
-NUMBER=$((1 + $RANDOM % 1000))
-
-# set number of guesses counter to 0
-COUNT=0
+PSQL="psql -X --username=$DB_USERNAME --dbname=$DB_NAME -t --no-align -c"
 
 WELCOME() {
   # prompt user for name
-  echo "Enter your username:"
+  echo -e "\nEnter your username:"
   read USERNAME
 
-  # check name length ?
-  if [[ ! $USERNAME =~ ^([a-zA-Z0-9_]){1,22}$ ]]
+  # get random number 1-1000
+  NUMBER=$((1 + $RANDOM % 1000))
+
+  # set number of guesses counter to 0
+  GUESS_COUNTER=0
+
+  #If that username has been used before, it should print
+  USER=$($PSQL "SELECT name FROM users WHERE name='$USERNAME'")
+  if [[ ! -z $USER ]]
   then
-    echo Username is too long.
+    echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+    INSERT_USER=$($PSQL "INSERT INTO users(name, best_game, games_played) VALUES('$USERNAME',NULL,NULL)")
   else
-    #get user name from db
+    # get user data
     USER_ID=$($PSQL "SELECT id_user FROM users WHERE name='$USERNAME'")
-    # if user exists
-    if [[ $USER_ID ]]
-    then
-      # get user data
-      GAMES_PLAYED=$($PSQL "SELECT COUNT(id_user) FROM game WHERE id_user=$USER_ID")
-      BEST_GAME=$($PSQL "SELECT MIN(best_game) FROM game WHERE id_user=$USER_ID")
-      # format variables removing spaces
-      GAMES_PLAYED_FMT=$(echo $GAMES_PLAYED | sed 's/^[[:blank:]]*//;s/[[:blank:]]*$//')
-      BEST_GAME_FMT=$(echo $BEST_GAME | sed 's/^[[:blank:]]*//;s/[[:blank:]]*$//')
-      # greet user
-      echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED_FMT games, and your best game took $BEST_GAME_FMT guesses."
-    else
-      # if user does not exist
-      echo -e "\nWelcome, $USERNAME It looks like this is your first time here."
-      # register user
-      INSERT_USER_RESULT=$($PSQL "INSERT INTO users(name) VALUES('$USERNAME')")
-      # if username is not created
-      if [[ $INSERT_USER_RESULT != "INSERT 0 1" ]]
-      then
-        echo -e "\nCould not register new user. Please try again."
-      else
-        # retrieve new user id
-        USER_ID=$($PSQL "SELECT id_user FROM users WHERE name='$USERNAME'")
-      fi
-    fi
-    # start playing
-    PLAY
+    USER_NAME_DB=$($PSQL "SELECT name FROM users WHERE name='$USERNAME'")
+    GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE id_user=$USER_ID")
+    BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE id_user=$USER_ID")
+    echo -e "\nWelcome back, $USER_NAME_DB! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   fi
+  PLAY
 }
 
 PLAY(){
+  # flag to end game
+  END=0
   # get guess number from user
   echo -e "\nGuess the secret number between 1 and 1000:"
-  read GUESS
-
-  # if guess is not a number
-  if [[ ! $GUESS =~ ^[0-9]+$ ]]
-  then
-    ((COUNT++))
-    echo -e "\nThat is not an integer, guess again:"    
-    PLAY
-  # if guess is less than secret number
-  elif [[ $GUESS -gt $NUMBER ]]
-  then
-    ((COUNT++))
-    echo -e "\nIt's lower than that, guess again:"      
-    PLAY
-  # if guess is higher than secret number
-  elif [[ $GUESS -lt $NUMBER ]]
-  then
-    ((COUNT++))
-    echo -e "\nIt's higher than that, guess again:"
-    PLAY
-  # if guess is correct
-  elif [[ $GUESS -eq $NUMBER ]]
-  then
-    ((COUNT++))
-    echo -e "\nYou guessed it in $COUNT tries. The secret number was $NUMBER. Nice job!"
-    # save user game data
-    INSERT_GAME_DATA_RESULT=$($PSQL "INSERT INTO game(id_user, best_game) VALUES($USER_ID,$COUNT)")
-  fi
+  
+  # Use a while loop instead of calling PLAY function again
+  while [[ $END -eq 0 ]]
+  do
+    # get guess from user
+    read GUESS
+    # if guess is not a number
+    if [[ ! $GUESS =~ ^[0-9]+$ ]]
+    then
+      ((GUESS_COUNTER++))
+      echo -e "\nThat is not an integer, guess again:"
+    # if guess is correct
+    elif [[ $GUESS -eq $NUMBER ]]
+    then
+      ((GUESS_COUNTER++))
+      echo -e "\nYou guessed it in $GUESS_COUNTER tries. The secret number was $NUMBER. Nice job!"
+      # save user game data
+      ((GAMES_PLAYED++))
+      SET_GAMES_PLAYED=$($PSQL "UPDATE users SET games_played=$GAMES_PLAYED WHERE name='$USERNAME'")
+      SET_BEST_GAME=$($PSQL "UPDATE users SET best_game=$GUESS_COUNTER WHERE name='$USERNAME' AND (best_game ISNULL OR $GUESS_COUNTER < best_game)")
+      END=1
+    # if guess is less than secret number
+    elif [[ $GUESS -gt $NUMBER ]]
+    then
+      ((GUESS_COUNTER++))
+      echo -e "\nIt's lower than that, guess again:"      
+      PLAY
+    # if guess is higher than secret number
+    elif [[ $GUESS -lt $NUMBER ]]
+    then
+      ((GUESS_COUNTER++))
+      echo -e "\nIt's higher than that, guess again:"
+      PLAY    
+    fi
+  done
 }
 
 WELCOME
