285731db9a0efb338d77c3e2e4197ce6a0d6be63
Carlos <134751214+cleall@users.noreply.github.com>
2023-06-26 12:35:04 -0600

    Update number_guess.sql
    Changed db structure to only use users table

--- number_guess.sql
+++ number_guess.sql
@@ -44,57 +44,24 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: game; Type: TABLE; Schema: public; Owner: freecodecamp
---
-
-CREATE TABLE public.game (
-    id_game integer NOT NULL,
-    id_user integer NOT NULL,
-    best_game integer
-);
-
-
-ALTER TABLE public.game OWNER TO freecodecamp;
-
---
--- Name: game_id_game_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
---
-
-CREATE SEQUENCE public.game_id_game_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.game_id_game_seq OWNER TO freecodecamp;
-
---
--- Name: game_id_game_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
---
-
-ALTER SEQUENCE public.game_id_game_seq OWNED BY public.game.id_game;
-
-
---
 -- Name: users; Type: TABLE; Schema: public; Owner: freecodecamp
 --
 
 CREATE TABLE public.users (
     id_user integer NOT NULL,
-    name character varying(22) NOT NULL
+    name character varying(22),
+    best_game integer,
+    games_played integer
 );
 
 
 ALTER TABLE public.users OWNER TO freecodecamp;
 
 --
--- Name: users_id_users_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+-- Name: users_id_user_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
 --
 
-CREATE SEQUENCE public.users_id_users_seq
+CREATE SEQUENCE public.users_id_user_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -103,33 +70,20 @@ CREATE SEQUENCE public.users_id_users_seq
     CACHE 1;
 
 
-ALTER TABLE public.users_id_users_seq OWNER TO freecodecamp;
+ALTER TABLE public.users_id_user_seq OWNER TO freecodecamp;
 
 --
--- Name: users_id_users_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+-- Name: users_id_user_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
 --
 
-ALTER SEQUENCE public.users_id_users_seq OWNED BY public.users.id_user;
-
-
---
--- Name: game id_game; Type: DEFAULT; Schema: public; Owner: freecodecamp
---
-
-ALTER TABLE ONLY public.game ALTER COLUMN id_game SET DEFAULT nextval('public.game_id_game_seq'::regclass);
+ALTER SEQUENCE public.users_id_user_seq OWNED BY public.users.id_user;
 
 
 --
 -- Name: users id_user; Type: DEFAULT; Schema: public; Owner: freecodecamp
 --
 
-ALTER TABLE ONLY public.users ALTER COLUMN id_user SET DEFAULT nextval('public.users_id_users_seq'::regclass);
-
-
---
--- Data for Name: game; Type: TABLE DATA; Schema: public; Owner: freecodecamp
---
-
+ALTER TABLE ONLY public.users ALTER COLUMN id_user SET DEFAULT nextval('public.users_id_user_seq'::regclass);
 
 
 --
@@ -139,33 +93,10 @@ ALTER TABLE ONLY public.users ALTER COLUMN id_user SET DEFAULT nextval('public.u
 
 
 --
--- Name: game_id_game_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+-- Name: users_id_user_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.game_id_game_seq', 23, true);
-
-
---
--- Name: users_id_users_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
---
-
-SELECT pg_catalog.setval('public.users_id_users_seq', 27, true);
-
-
---
--- Name: game game_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
---
-
-ALTER TABLE ONLY public.game
-    ADD CONSTRAINT game_pkey PRIMARY KEY (id_game);
-
-
---
--- Name: users users_name_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_name_key UNIQUE (name);
+SELECT pg_catalog.setval('public.users_id_user_seq', 40, true);
 
 
 --
@@ -174,14 +105,6 @@ ALTER TABLE ONLY public.users
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id_user);
-
-
---
--- Name: game game_id_user_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
---
-
-ALTER TABLE ONLY public.game
-    ADD CONSTRAINT game_id_user_fkey FOREIGN KEY (id_user) REFERENCES public.users(id_user);
 
 
 --
