e7d42f912b99a53034b4c0ea455a8e4abdde109f
Carlos <134751214+cleall@users.noreply.github.com>
2023-06-28 19:18:50 -0600

    code road tests logs
    uploaded for review

---
+++ code_road_tests_logs.js
@@ -0,0 +1,78 @@
+FAILED TEST LOG
+
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+  ✘ SUBTASKS 1.1 :8 Your script should print the correct welcome message for returning users
+
+Error: write EPIPE
+at WriteWrap.onWriteComplete [as oncomplete] (internal/stream_base_commons.js:94:16)
+  ✘ SUBTASKS 1.1 :8 Your script should print the correct welcome message for returning users
+
+Error: done() called multiple times in test <SUBTASKS 1.1 :8 Your script should print the correct welcome message for returning users> of file /home/codeally/project/.freeCodeCamp/test/1.1.test.js; in addition, done() received error: AssertionError [ERR_ASSERTION]: The expression evaluated to a falsy value:
+
+assert(re.test(scriptOutput))
+
+at processTicksAndRejections (internal/process/task_queues.js:95:5)
+
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+  ✘ SUBTASKS 1.1 :9 Your script should print the correct welcome message for new users
+
+Error: write EPIPE
+at WriteWrap.onWriteComplete [as oncomplete] (internal/stream_base_commons.js:94:16)
+  ✘ SUBTASKS 1.1 :9 Your script should print the correct welcome message for new users
+
+Error: done() called multiple times in test <SUBTASKS 1.1 :9 Your script should print the correct welcome message for new users> of file /home/codeally/project/.freeCodeCamp/test/1.1.test.js
+at runMicrotasks (<anonymous>)
+at processTicksAndRejections (internal/process/task_queues.js:95:5)
+
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+  ✘ SUBTASKS 1.1 :12 Your script should print the correct message if users enter a guess other than a number
+
+Error: write EPIPE
+at WriteWrap.onWriteComplete [as oncomplete] (internal/stream_base_commons.js:94:16)
+  ✘ SUBTASKS 1.1 :12 Your script should print the correct message if users enter a guess other than a number
+
+Error: done() called multiple times in test <SUBTASKS 1.1 :12 Your script should print the correct message if users enter a guess other than a number> of file /home/codeally/project/.freeCodeCamp/test/1.1.test.js
+at runMicrotasks (<anonymous>)
+at processTicksAndRejections (internal/process/task_queues.js:95:5)
+
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+An error occurred running ./number_guessing_game/number_guess.sh
+psql: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "codeally" does not exist
+  ✘ SUBTASKS 1.1 :13 Your script should print the correct message when a game is finished
+
+Error: write EPIPE
+at WriteWrap.onWriteComplete [as oncomplete] (internal/stream_base_commons.js:94:16)
+  ✘ SUBTASKS 1.1 :13 Your script should print the correct message when a game is finished
+
+Error: done() called multiple times in test <SUBTASKS 1.1 :13 Your script should print the correct message when a game is finished> of file /home/codeally/project/.freeCodeCamp/test/1.1.test.js; in addition, done() received error: AssertionError [ERR_ASSERTION]: The expression evaluated to a falsy value:
+
+assert(re.test(scriptOutput))
+
+at runMicrotasks (<anonymous>)
+at processTicksAndRejections (internal/process/task_queues.js:95:5)
